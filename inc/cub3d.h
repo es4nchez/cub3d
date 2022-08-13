fa5ee8c978e68d8ebc2141060e5000530eea5087
Eduardo Sanchez <esanchez@student.42lausanne.ch>
2022-08-13 20:05:10 +0200

    merging parsing + minimap

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -24,6 +24,8 @@
 # include <string.h>
 # include <math.h>
 
+#include "../libft/libft.h"
+
 #define WIN_W 5120 / 4
 #define WIN_H 2880 / 4
 
@@ -97,6 +99,13 @@ typedef struct s_data {
 	float		dirY;
 	float		speed;
 	int			minimapSize;
+
+	char		*no;
+	char		*so;
+	char		*we;
+	char		*ea;
+	char		*f;
+	char		*c;
 
 }				t_data;
 
